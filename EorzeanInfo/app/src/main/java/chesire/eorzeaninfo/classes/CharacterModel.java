1e4f8061234f8c7ae93bc49d6c5d4691273a7bca
Trey <troyrijkaard@hotmail.co.uk>
2017-05-07 00:00:40 +0100

    wip: hide some model data for the characters

--- EorzeanInfo/app/src/main/java/chesire/eorzeaninfo/classes/CharacterModel.java
+++ EorzeanInfo/app/src/main/java/chesire/eorzeaninfo/classes/CharacterModel.java
@@ -1,13 +1,21 @@
 package chesire.eorzeaninfo.classes;
 
 public class CharacterModel {
-    public String icon;
-    public int id;
-    public String last_updated;
-    public String name;
-    public String server;
-    public String url;
-    public String url_api;
-    public String url_type;
-    public String url_xivdb;
+    private String icon;
+    private int id;
+    private String last_updated;
+    private String name;
+    private String server;
+    private String url;
+    private String url_api;
+    private String url_type;
+    private String url_xivdb;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getServer() {
+        return server;
+    }
 }
