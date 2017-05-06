d8fc35c9a529795b84364feaf705a420ec56f8c7
Trey <troyrijkaard@hotmail.co.uk>
2017-05-06 19:54:02 +0100

    Initial commit

---
+++ EorzeanInfo/app/src/main/java/chesire/eorzeaninfo/CharacterSelect.java
@@ -0,0 +1,13 @@
+package chesire.eorzeaninfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharacterSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_select);
+    }
+}
