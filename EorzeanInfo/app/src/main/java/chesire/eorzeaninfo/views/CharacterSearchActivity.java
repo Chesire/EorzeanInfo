59d56f4f059af57f54e755e1935a81996cd4d410
Trey <troyrijkaard@hotmail.co.uk>
2017-05-07 01:05:03 +0100

    wip: add spinners for selecting the data centre and server

--- EorzeanInfo/app/src/main/java/chesire/eorzeaninfo/views/CharacterSearchActivity.java
+++ EorzeanInfo/app/src/main/java/chesire/eorzeaninfo/views/CharacterSearchActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.support.design.BuildConfig;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import javax.inject.Inject;
 
+import butterknife.BindArray;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnItemSelected;
 import chesire.eorzeaninfo.EorzeanInfoApp;
 import chesire.eorzeaninfo.R;
 import chesire.eorzeaninfo.interfaces.XIVDBService;
@@ -23,8 +28,14 @@ public class CharacterSearchActivity extends AppCompatActivity {
 
     @BindView(R.id.character_search_name_input)
     TextInputEditText mCharacterNameInput;
-    @BindView(R.id.character_search_server_input)
-    TextInputEditText mCharacterServerInput;
+    @BindView(R.id.character_search_data_centre_selection)
+    AppCompatSpinner mDataCentreSelector;
+    @BindView(R.id.character_search_server_selection)
+    AppCompatSpinner mCharacterServerSelector;
+
+    @BindArray(R.array.data_centres)
+    String[] mDataCentres;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +45,68 @@ public class CharacterSearchActivity extends AppCompatActivity {
         ((EorzeanInfoApp) getApplication()).getXIVComponent().inject(this);
         ButterKnife.bind(this);
 
+        ArrayAdapter<String> dataCentreAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, mDataCentres);
+        mDataCentreSelector.setAdapter(dataCentreAdapter);
+
         if (BuildConfig.DEBUG) {
             mCharacterNameInput.setText("Cheshire Cat");
-            mCharacterServerInput.setText("Phoenix");
         }
+    }
+
+    @OnItemSelected(R.id.character_search_data_centre_selection)
+    void onItemSelected() {
+        String[] selectedServerList;
+        String selectedDataCentre = (String) mDataCentreSelector.getSelectedItem();
+        switch (selectedDataCentre) {
+            case "Any":
+                selectedServerList = new String[0];
+                break;
+
+            case "Elemental":
+                selectedServerList = getResources().getStringArray(R.array.elemental_servers);
+                break;
+
+            case "Gaia":
+                selectedServerList = getResources().getStringArray(R.array.gaia_servers);
+                break;
+
+            case "Mana":
+                selectedServerList = getResources().getStringArray(R.array.mana_servers);
+                break;
+
+            case "Aether":
+                selectedServerList = getResources().getStringArray(R.array.aether_servers);
+                break;
+
+            case "Primal":
+                selectedServerList = getResources().getStringArray(R.array.primal_servers);
+                break;
+
+            case "Chaos":
+                selectedServerList = getResources().getStringArray(R.array.chaos_servers);
+                break;
+
+            default:
+                throw new IndexOutOfBoundsException(String.format("Unexpected value encountered - %1$s", selectedDataCentre));
+        }
+
+        mCharacterServerSelector.setVisibility(selectedServerList.length == 0 ? View.INVISIBLE : View.VISIBLE);
+        ArrayAdapter<String> serverAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, selectedServerList);
+        mCharacterServerSelector.setAdapter(serverAdapter);
     }
 
     @OnClick(R.id.character_search_button)
     void searchClicked() {
+        String selectedServer;
+        Object selectedItem = mCharacterServerSelector.getSelectedItem();
+        if (selectedItem == null) {
+            selectedServer = null;
+        } else {
+            selectedServer = (String) selectedItem;
+        }
+
         try {
-            Call<XIVDBService.SearchCharactersResponse> charCall = mXIVClient.searchCharacters(mCharacterServerInput.getEditableText().toString(), mCharacterNameInput.getEditableText().toString());
+            Call<XIVDBService.SearchCharactersResponse> charCall = mXIVClient.searchCharacters(selectedServer, mCharacterNameInput.getEditableText().toString());
             charCall.enqueue(new Callback<XIVDBService.SearchCharactersResponse>() {
                 @Override
                 public void onResponse(Call<XIVDBService.SearchCharactersResponse> call, Response<XIVDBService.SearchCharactersResponse> response) {
