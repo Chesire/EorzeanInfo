d32422b6f16bcb54aae1893d14284f518cc82276
Trey <troyrijkaard@hotmail.co.uk>
2017-05-09 00:30:26 +0100

    wip: make the CharacterModel parcelable

--- EorzeanInfo/app/src/main/java/chesire/eorzeaninfo/classes/CharacterModel.java
+++ EorzeanInfo/app/src/main/java/chesire/eorzeaninfo/classes/CharacterModel.java
@@ -1,6 +1,9 @@
 package chesire.eorzeaninfo.classes;
 
-public class CharacterModel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class CharacterModel implements Parcelable {
     private String icon;
     private int id;
     private String last_updated;
@@ -11,6 +14,18 @@ public class CharacterModel {
     private String url_type;
     private String url_xivdb;
 
+    protected CharacterModel(Parcel in) {
+        icon = in.readString();
+        id = in.readInt();
+        last_updated = in.readString();
+        name = in.readString();
+        server = in.readString();
+        url = in.readString();
+        url_api = in.readString();
+        url_type = in.readString();
+        url_xivdb = in.readString();
+    }
+
     public String getName() {
         return name;
     }
@@ -18,4 +33,36 @@ public class CharacterModel {
     public String getServer() {
         return server;
     }
+
+    @Override
+    public int describeContents() {
+        // auto generated
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(icon);
+        dest.writeInt(id);
+        dest.writeString(last_updated);
+        dest.writeString(name);
+        dest.writeString(server);
+        dest.writeString(url);
+        dest.writeString(url_api);
+        dest.writeString(url_type);
+        dest.writeString(url_xivdb);
+    }
+
+    public static final Creator<CharacterModel> CREATOR = new Creator<CharacterModel>() {
+        @Override
+        public CharacterModel createFromParcel(Parcel in) {
+            return new CharacterModel(in);
+        }
+
+        @Override
+        public CharacterModel[] newArray(int size) {
+            return new CharacterModel[size];
+        }
+    };
+
 }
