d86a698b17a7b1d0e6b95677d8cc3589f644e434
Trey <troyrijkaard@hotmail.co.uk>
2017-05-13 17:52:58 +0100

    wip: add buttons for firstLaunch

--- EorzeanInfo/app/src/main/java/chesire/eorzeaninfo/views/FirstLaunchActivity.java
+++ EorzeanInfo/app/src/main/java/chesire/eorzeaninfo/views/FirstLaunchActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.OnClick;
 import chesire.eorzeaninfo.R;
 
 public class FirstLaunchActivity extends AppCompatActivity {
@@ -17,5 +18,15 @@ public class FirstLaunchActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
+    }
+
+    @OnClick(R.id.first_launch_skip_button)
+    void onSkip() {
+
+    }
+
+    @OnClick(R.id.first_launch_next_button)
+    void onNext() {
+        
     }
 }
