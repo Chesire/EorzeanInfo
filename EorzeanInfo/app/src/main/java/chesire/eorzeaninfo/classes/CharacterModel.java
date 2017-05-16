ed550400c62388adf4c24537e1ce2e941f0c60ad
Trey <troyrijkaard@hotmail.co.uk>
2017-05-16 21:28:27 +0100

    wip: reduce stored fields on the CharacterModel

--- EorzeanInfo/app/src/main/java/chesire/eorzeaninfo/classes/CharacterModel.java
+++ EorzeanInfo/app/src/main/java/chesire/eorzeaninfo/classes/CharacterModel.java
@@ -9,24 +9,15 @@ import android.os.Parcelable;
 public class CharacterModel implements Parcelable {
     private String icon;
     private int id;
-    private String last_updated;
     private String name;
     private String server;
-    private String url;
-    private String url_api;
-    private String url_type;
-    private String url_xivdb;
+    private String title;
 
     protected CharacterModel(Parcel in) {
         icon = in.readString();
         id = in.readInt();
-        last_updated = in.readString();
         name = in.readString();
         server = in.readString();
-        url = in.readString();
-        url_api = in.readString();
-        url_type = in.readString();
-        url_xivdb = in.readString();
     }
 
     /**
@@ -76,13 +67,8 @@ public class CharacterModel implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(icon);
         dest.writeInt(id);
-        dest.writeString(last_updated);
         dest.writeString(name);
         dest.writeString(server);
-        dest.writeString(url);
-        dest.writeString(url_api);
-        dest.writeString(url_type);
-        dest.writeString(url_xivdb);
     }
 
     public static final Creator<CharacterModel> CREATOR = new Creator<CharacterModel>() {
